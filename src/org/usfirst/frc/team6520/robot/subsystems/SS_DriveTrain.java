65dd124acf0e4022f61bab630f4141c1e95b98aa
Tom-from-6520 <tapplayer16@gmail.com>
2017-01-09 23:37:01 +0700

    1st build

---
+++ src/org/usfirst/frc/team6520/robot/subsystems/SS_DriveTrain.java
@@ -0,0 +1,34 @@
+
+package org.usfirst.frc.team6520.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class SS_DriveTrain extends Subsystem {
+
+	final int FRONT_RIGHT_MOTOR_PORT = 1;
+	final int FRONT_LEFT_MOTOR_PORT = 4;
+	final int BACK_RIGHT_MOTOR_PORT = 0;
+	final int BACK_LEFT_MOTOR_PORT = 3;
+	
+	VictorSP FrontLeftMotor = new VictorSP(FRONT_LEFT_MOTOR_PORT);
+	VictorSP FrontRightMotor = new VictorSP(FRONT_RIGHT_MOTOR_PORT);
+	VictorSP BackLeftMotor = new VictorSP(BACK_LEFT_MOTOR_PORT);
+	VictorSP BackRightMotor = new VictorSP(BACK_RIGHT_MOTOR_PORT);
+
+	public void driveByJoystick(double power) {
+		FrontLeftMotor.set(power);
+    	BackLeftMotor.set(power);
+    	
+		FrontRightMotor.set(-power);
+		BackRightMotor.set(-power);
+    }
+	
+    public void initDefaultCommand() {
+    }
+}
+
